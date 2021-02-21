215324e1c8fce3ee3c331c3cb95c5552e00f9ed1
coopzone-dc <70201105+coopzone-dc@users.noreply.github.com>
2021-02-21 14:30:14 +0000

    Add files via upload

---
+++ rtc.asm
@@ -0,0 +1,98 @@
+;Test user LED and key
+;
+BDOS	equ	5	;cp/m 2.2 BDOS entry point
+TMDT	equ	132	;Time/Date optcode
+DPORT	equ	0	;data i/o port
+CPORT	equ	1	;Command control port
+;
+	org	100h
+main:
+	lxi	h,0	;keeping old stack to avoid wboot on exit
+	dad	sp	;
+	shld	(oldSP)
+	lxi	sp,stackend
+;
+	mvi	a,TMDT	;Get the time,date and temp optcode
+	out	CPORT
+	in	DPORT	;get seconds
+	lxi	h,sec	;where to store it
+	call	savit
+	in	DPORT	;get seconds
+	lxi	h,min	;where to store it
+	call	savit
+	in	DPORT	;get seconds
+	lxi	h,hours	;save it
+	call	savit
+	in	DPORT	;get seconds
+	lxi	h,day	;where to store it
+	call	savit
+	in	DPORT	;get seconds
+	lxi	h,month	;where to store it
+	call	savit
+	in	DPORT	;year
+	lxi	h,year	;save it here
+	call	savit
+	in	DPORT	;temp
+	lxi	h,temp	; and save it here
+	cpi	128	; check to see if it's negative
+	jm	tempok	;if less than 128
+	cma
+	inr	a	;2's compl
+tempok:
+	call	savit	;store the  temp
+	lxi	h,str	;print the results
+	call	puts		
+exit:	
+	lhld	(oldSP)	;get back the old stack
+	sphl		;into stack
+	ret		;exit without wboot
+;
+puts:
+	mov	a,m	;get the char to print
+	ora	a	;is it zero
+	rz		;return if zero, end of message
+	mvi	c,2	;console output
+	mov	e,a	;char to print
+	push	h
+	call	BDOS	;print it
+	pop	h	;restor local regs
+	inx	h
+	jmp	puts	;next char to print
+;
+savit:
+	mvi	b,'0'	;hold the result of the divide,start at ascii 0
+savnxt:	xri	0	;clear carry
+	sbi	10	;subtract 10
+	jm	savdon	;if it cause a borrow then done
+	inr	b	;add up the 10's
+	jmp	savnxt	;keep going
+savdon:	adi	'0'+10	;add back the last 10 + ascii 0
+	mov	m,b	;save the 10's units
+	inx	h
+	mov	m,a	;save the one's
+	ret
+;
+	
+;
+str	db	'Current time: '
+hours	db	'hh'
+	db	':'
+min	db	'mm'
+	db	':'
+sec	db	'ss'
+	db	' '
+day	dw	0
+	db	'/'
+month	dw	0
+	db	'/'
+year	dw	0
+	db	0ah,0dh
+	db	'Temperature:  '
+temp	dw	0
+	db	'C'
+	db	0ah,0dh,0
+oldSP:	ds	2
+stack:	ds	64
+stackend:	equ	$
+
+
